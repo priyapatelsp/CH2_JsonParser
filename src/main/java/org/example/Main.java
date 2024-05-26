3840e1c6bab1eab6f2be23903088850a6c23434c
Priya Patel <priyapatelsp3012@gmail.com>
2024-05-26 18:47:17 -0400

    added required changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         System.out.println("::::   Welcome to JSON Parser   ::::");
         while(true){
